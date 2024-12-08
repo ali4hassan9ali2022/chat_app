3387a3ee9f95525a6cd3e0b55d908c9dc7536954
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-08 18:52:20 +0200

    create auth state

---
+++ lib/cubit/auth_cubit/auth_state.dart
@@ -0,0 +1,13 @@
+abstract class AuthState {}
+class LoginLoadingState extends AuthState{}
+class LoginSuccessState extends AuthState{}
+class LoginFailureState extends AuthState{
+  final String errmessage;
+  LoginFailureState({required this.errmessage});
+}
+class RegisterLoadingState extends AuthState{}
+class RegisterSuccessState extends AuthState{}
+class RegisterFailureState extends AuthState{
+  final String errmessage;
+  RegisterFailureState({required this.errmessage});
+}

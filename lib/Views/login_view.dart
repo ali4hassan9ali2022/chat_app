4b8177d26db0196a70fdbcf7c0d9c332246633b2
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-08 18:46:55 +0200

    create validdata

--- lib/Views/login_view.dart
+++ lib/Views/login_view.dart
@@ -3,92 +3,119 @@ import 'package:chat_app/Widgets/custom_botton.dart';
 import 'package:chat_app/Widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
   static String id = "LoginView";
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? email;
+  String? password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ListView(
-          children: [
-            const SizedBox(
-              height: 75,
-            ),
-            Image.asset(
-              "assets/images/scholar.png",
-              height: 100,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Scholar",
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontFamily: "Pacifico",
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            const Text(
-              "Login",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
+        child: Form(
+          key: formKey,
+          autovalidateMode: autovalidateMode,
+          child: ListView(
+            children: [
+              const SizedBox(
+                height: 75,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const CustomTextFormField(
-              icon: Icons.person,
-              text: "Email",
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const CustomTextFormField(
-              text: "Password",
-              icon: Icons.password,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomBotton(
-              onTap: () {},
-              text: "login",
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'dont\'t have an account?',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, RegisterView.id);
-                  },
-                  child: const Text(
-                    '  Register',
+              Image.asset(
+                "assets/images/scholar.png",
+                height: 100,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Scholar",
                     style: TextStyle(
-                      color: Color(0xffC7EDE6),
+                      fontSize: 22,
+                      fontFamily: "Pacifico",
                     ),
                   ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                "Login",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomTextFormField(
+                onSaved: (value) {
+                  email = value;
+                },
+                icon: Icons.person,
+                text: "Email",
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomTextFormField(
+                onSaved: (value) {
+                  password = value;
+                },
+                text: "Password",
+                icon: Icons.password,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomBotton(
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+                text: "login",
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'dont\'t have an account?',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, RegisterView.id);
+                    },
+                    child: const Text(
+                      '  Register',
+                      style: TextStyle(
+                        color: Color(0xffC7EDE6),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/Widgets/custom_text_form_field.dart
+++ lib/Widgets/custom_text_form_field.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
   const CustomTextFormField(
-      {super.key, required this.text, required this.icon});
+      {super.key, required this.text, required this.icon, this.onSaved});
   final String text;
   final IconData icon;
+  final Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "Required";
+        } else {
+          return null;
+        }
+      },
+      onSaved: onSaved,
       decoration: InputDecoration(
         hintText: text,
         suffixIcon: Icon(icon),

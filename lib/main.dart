086ac800cab87d798d767d8fd79da0c8342f20eb
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-08 18:39:47 +0200

    create firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:chat_app/Views/login_view.dart';
 import 'package:chat_app/Views/register_view.dart';
+import 'package:chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ChatApp());
 }
 
@@ -15,8 +21,8 @@ class ChatApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       routes: {
-        LoginView.id: (context)=> const LoginView(),
-        RegisterView.id: (context)=> const RegisterView(),
+        LoginView.id: (context) => const LoginView(),
+        RegisterView.id: (context) => const RegisterView(),
       },
       initialRoute: LoginView.id,
     );
